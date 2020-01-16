3e95294a8acafe5385c06179b5e45603283e2e84
Raul Rosa da Silva <raul.silva@cwi.com.br>
2020-01-16 20:44:10 -0300

    add copy to clipboard

--- js/main.js
+++ js/main.js
@@ -23,14 +23,19 @@ const selectors = {
 
 function addPhrasesOnList(phrases, addStorage) {
     if (phrases) {
+        var concatPhrases = "";
         for (var i = 0; i < phrases.length; i++) {
             if (phrases[i]) {
                 selectors.seletorListPhrases.append("<l1 class='item-phrase'>" + phrases[i] + "</li>");
+
+                concatPhrases += " " + phrases[i];
 
                 if (addStorage)
                     storage.savePhrasesStorage(phrases[i]);
             }
         }
+
+        copyToClipboard(concatPhrases);
     }
 }
 
@@ -72,3 +77,25 @@ function configuracoesStorage() {
         return data;
     });
 }
+
+const copyToClipboard = str => {
+    if (str) {
+        const el = document.createElement('textarea');  // Create a <textarea> element
+        el.value = str;                                 // Set its value to the string that you want copied
+        el.setAttribute('readonly', '');                // Make it readonly to be tamper-proof
+        el.style.position = 'absolute';
+        el.style.left = '-9999px';                      // Move outside the screen to make it invisible
+        document.body.appendChild(el);                  // Append the <textarea> element to the HTML document
+        const selected =
+            document.getSelection().rangeCount > 0        // Check if there is any content selected previously
+                ? document.getSelection().getRangeAt(0)     // Store selection if found
+                : false;                                    // Mark as false to know no selection existed before
+        el.select();                                    // Select the <textarea> content
+        document.execCommand('copy');                   // Copy - only works as a result of a user action (e.g. click events)
+        document.body.removeChild(el);                  // Remove the <textarea> element
+        if (selected) {                                 // If a selection existed before copying
+            document.getSelection().removeAllRanges();    // Unselect everything on the HTML document
+            document.getSelection().addRange(selected);   // Restore the original selection
+        }
+    }
+};
